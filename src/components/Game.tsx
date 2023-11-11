73056e04c94053cf85063b931b2a474f29e4995c
abdirrahman <abdirrahmang@gmail.com>
2023-11-11 19:31:47 +0000

    MVP established.

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -3,25 +3,23 @@ import { Button } from "./ui/button";
 import * as React from "react";
 import { Toaster, toast } from "sonner";
 import { countryListAlpha2 } from "@/lib/countries";
-import { Score } from "./Score";
-import Helper from "./Helper";
 
-export default function MyForm() {
+export default function Game() {
   const [countryCode, setCountryCode] = React.useState<string>("cz");
   const [countryName, setCountryName] = React.useState<string>("Czechia");
-  // const score = React.useRef<number>(0);
   const [score, setScore] = React.useState<number>(0);
   const [answer, setAnswer] = React.useState<string>("");
-  const r = React.useRef<any>();
-  // const guess = React.useRef<any>();
 
   React.useEffect(() => {
-    // gameSetup();
-    console.log(countryName);
+    // fetch("https://flagcdn.com/en/codes.json")
+    //   .then((response) => response.json())
+    //   .then((data) => console.log(data))
+    //   .catch((error) => console.error(error));
+    gameSetup();
   }, []);
 
   function gameSetup() {
-    const country = getRandomCountryCode(countryListAlpha2);
+    const country = getRandomCountry(countryListAlpha2);
     if (country === undefined) {
       console.log("Undefined country");
     } else {
@@ -36,33 +34,26 @@ export default function MyForm() {
     const form = e.target;
     const formData = new FormData(form);
     // Read the form data
-
     const formJson = Object.fromEntries(formData.entries());
-    console.log(formJson.guess);
-    console.log(score);
 
-    // const formData = new FormData(form);
-
-    // You can pass formData as a fetch body directly:
-    // fetch("/some-api", { method: form.method, body: formData });
-
-    // Or you can work with it as a plain object:
-    // const formJson = Object.fromEntries(formData.entries());
-    // console.log(formJson.myInput);
-    if (formJson.guess == countryName) {
+    if (
+      formJson.guess.toString().toLowerCase() ==
+      removeThe(countryName.toLowerCase())
+    ) {
       setScore(score + 1);
       toast.success("Correct!");
       console.log("correct");
       gameSetup();
     } else {
-      toast.error("Incorrect!");
+      toast.error(`Incorrect! That was ${removeThe(countryName)}.`);
+      console.log(removeThe(countryName));
       gameSetup();
       formJson.guess = "";
     }
+    // Reset input
     setAnswer("");
   }
-
-  function getRandomCountryCode(
+  function getRandomCountry(
     obj: Record<string, string>
   ): [string, string] | undefined {
     const keys = Object.keys(obj);
@@ -76,14 +67,23 @@ export default function MyForm() {
     console.log(randomKey, randomValue);
     return [randomKey, randomValue];
   }
+  function removeThe(input: string): string {
+    // Use regular expression to match "(the)" case insensitive globally
+    const regex = /\(the\)/gi;
+
+    // Replace "(the)" occurrences with an empty string
+    const result = input.replace(regex, "");
+
+    return result.trim();
+  }
 
   return (
     <>
-      <Helper
-        countryCode={countryCode}
-        countryName={countryName}
-        answer={answer}
-        points={score}
+      <div className="absolute top-0 right-0 ">{score}</div>
+      <img
+        src={`https://flagcdn.com/${countryCode}.svg`}
+        width="200"
+        className="p-2"
       />
       <form method="post" onSubmit={handleSubmit}>
         <label>
@@ -94,17 +94,9 @@ export default function MyForm() {
             onChange={(e) => setAnswer(e.target.value)}
             placeholder="Guess Here"
           />
-
-          {/* <Input
-            value={answer}
-            onChange={(e) => setAnswer(e.target.value)}
-            placeholder="Guess Here"
-          /> */}
         </label>
       </form>
-
       <Toaster richColors position="top-center" />
-      {/* <Button onClick={() => setScore(score + 1)}>CLick - {score}</Button> */}
     </>
   );
 }
